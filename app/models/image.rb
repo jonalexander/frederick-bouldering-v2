555155cc99ed73c33e59cca11732638cabdc2adb
Jonathan Alexander <jonathandalexander@gmail.com>
2016-09-23 15:20:16 -0400

    set perform_caching to false in config

--- app/models/image.rb
+++
@@ -1,7 +0,0 @@
-class Image < ApplicationRecord
-  belongs_to :user
-  belongs_to :bproblem
-
-  mount_uploader :image, ImageUploader
-  # ImageUploader to populate :image column on Images table
-end
